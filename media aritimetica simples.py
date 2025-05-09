e3645fa7be03fd126997d9385850ee843e35123c
DavidMaciel-DEV <davidsmaciel7@gmail.com>
2025-05-09 15:03:35 -0300

    tabuada updates

--- media aritimetica simples.py
+++ media aritimetica simples.py
@@ -1,6 +1,15 @@
+#Média Aritimetica Simples
 Valor1 = int(input("Digite o primeiro número: "))
 Valor2 = int(input("Digite o segundo número: "))
 valor3 = int(input("Digite o quarto número: "))
 Valor4 = int(input("Digite o quarto número: "))
 As = (Valor1 + Valor2 + valor3+ Valor4) / 4
 print("A média aritimetica simples é", As)
+
+#Média Aritimetica Geometrica
+Ag = Valor1 * Valor2 * valor3 * Valor4 **(1/2)
+print("A média aritimetica geometrica é", Ag)
+
+#Média Aritimetica Harmonica
+Ah = (1 / Valor1 + 1/Valor2 + 1/valor3 + 1/Valor4) / 4
+print("A média aritimetica harmonica é", Ah)
